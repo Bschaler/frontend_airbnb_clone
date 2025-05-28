0af9be3e66d0a610ebef33703d21e8f616c4d864
Bschaler <90104387+Bschaler@users.noreply.github.com>
2025-05-28 13:37:46 -0400

    Fix API URL for production

--- backend/db/seeders/20250101000000-demo-user.js
+++ backend/db/seeders/20250101000000-demo-user.js
@@ -1,37 +1,48 @@
 'use strict';
-const { User } = require('../models');
 const bcrypt = require('bcryptjs');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await User.bulkCreate([
+    const options = process.env.NODE_ENV === 'production' ? {
+      schema: process.env.SCHEMA
+    } : {};
+
+    await queryInterface.bulkInsert('Users', [
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
         firstName: 'Frodo',
         lastName: 'Baggins',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
         firstName: 'Jarvis',
         lastName: 'Script',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
         firstName: 'Terry',
         lastName: 'Dactyl',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
-    ], { validate: true });
+    ], options);
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', {
-      username: { [Sequelize.Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    const options = process.env.NODE_ENV === 'production' ? {
+      schema: process.env.SCHEMA
+    } : {};
+    
+    await queryInterface.bulkDelete('Users', null, options);
   }
 };
